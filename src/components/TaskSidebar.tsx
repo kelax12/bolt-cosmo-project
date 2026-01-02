eeeafb3302653b28097300d8f879bbcd21c0641d
kelax12 <kelaxbusiness@gmail.com>
2026-01-02 20:55:11 +0100

    Update TaskSidebar.tsx

--- src/components/TaskSidebar.tsx
+++ src/components/TaskSidebar.tsx
@@ -28,7 +28,7 @@ const TaskSidebar: React.FC<TaskSidebarProps> = ({ onClose }) => {
 
   const getPriorityColor = (priority: number) => {
     const colors = {
-      1: 'bg-red-100 text-red-800',
+      1: 'bg-red-200 text-red-900',
       2: 'bg-orange-100 text-orange-800',
       3: 'bg-yellow-100 text-yellow-800',
       4: 'bg-blue-100 text-blue-800',
